3b998859b96a321eafd40f52b792f34c34ce2dbf
RAJ <bibhanshuraj19@gmail.com>
2025-06-10 21:56:59 +0530

    Add new file(s)

---
+++ TestCodes/feature_scaling.py
@@ -0,0 +1,23 @@
+# Import necessary libraries
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+# Load the Wine Quality Red dataset
+df = pd.read_csv('winequality-red.csv',delimiter=';')
+# Separate features and target
+X = df.drop('quality',axis=1)
+y = df['quality']
+# Split the dataset into an 80-20 training-test set
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+# Create an instance of the StandardScaler class
+scaler = StandardScaler()
+
+# Fit the StandardScaler on the features from the training set and transform it
+X_train = scaler.fit_transform(X_train)
+# Apply the transform to the test set
+X_test = scaler.transform(X_test)
+# Print the scaled training and test datasets
+print(X_train)
+print(X_test)
